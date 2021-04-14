8cf0473467f714a86f764e9ecd78cc668e1bd6ca
watasuke102 <watasuke102@gmail.com>
2021-04-14 22:59:08 +0900

    update: Reactの環境を整えた

---
+++ src/index.tsx
@@ -0,0 +1,16 @@
+/*!
+ * index.tsx
+ *
+ * CopyRight (c) 2021 Watasuke
+ * Email  : <watasuke102@gmail.com>
+ * Twitter: @Watasuke102
+ * This software is released under the MIT SUSHI-WARE License.
+ */
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+  <p>hello</p>,
+  document.getElementById('main')
+);
---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: "development",
+  entry: './src/index.tsx',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  devServer: { port: 3000, hot: true, open: true },
+
+  module: {
+    rules: [
+      {
+        test: /\.js(x?)$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.ts(x?)$/,
+        loader: 'ts-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({ template: './src/index.html' })
+  ],
+  resolve: {
+    extensions: ['.js', '.ts', '.tsx']
+  }
+}
