797ad761c44e048f4617fb719c18b812f904442c
watasuke102 <watasuke102@gmail.com>
2021-04-26 22:39:28 +0900

    update: ヘッダーを追加

---
+++ src/components/Header.tsx
@@ -0,0 +1,27 @@
+/*!
+ * Header.tsx
+ *
+ * CopyRight (c) 2021 Watasuke
+ * Email  : <watasuke102@gmail.com>
+ * Twitter: @Watasuke102
+ * This software is released under the MIT SUSHI-WARE License.
+ */
+
+import React from 'react';
+import { Link } from 'gatsby';
+import '../styles/Header.scss'
+
+export default () => {
+  return (
+    <div className='Header-container'>
+      <div className='Header-title'>
+        <Link to='/' >わたすけのへや</Link>
+      </div>
+      <div className='Header-links'>
+        <Link to='/blog'>Blog</Link>
+        <Link to='/profile'>Profile</Link>
+        <Link to='/about'>About this site</Link>
+      </div>
+    </div>
+  )
+}
