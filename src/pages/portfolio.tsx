3c10ce3994177454a1b5dc2e812a86d2c0077358
watasuke102 <watasuke102@gmail.com>
2021-04-28 22:39:26 +0900

    update: スクロール状態のインジケータを表示できるように ポートフォリオ用

--- src/pages/portfolio.tsx
+++ src/pages/portfolio.tsx
@@ -7,16 +7,65 @@
  * This software is released under the MIT SUSHI-WARE License.
  */
 
-import { Link } from 'gatsby';
 import React from 'react';
+import '../styles/portfolio.scss'
 
-export default () => {
-  return (
-    <div>
-      製作中<br />
-      <Link to='/blog'>ブログ</Link>などはどうでしょうか
-      <br /><br />
-      <Link to='/'>トップにもどる</Link>
-    </div>
-  )
+enum Position {
+  none,
+  reached_bottom,
+  reached_top,
+}
+
+interface States {
+  current_page: number,
+  scroll_height: number,
+  place: Position
+}
+
+export default class Portfolio extends React.Component<any, States> {
+  constructor(props: any) {
+    super(props);
+    this.state = { current_page: 0, scroll_height: 0, place: Position.none };
+  }
+
+  UpdateScrollBar() {
+    const container = document.getElementById('portfolio-container');
+    if (!container) return;
+    // 画面全体に対するスクロール量の割合
+    const percent = container.scrollTop / (container.scrollHeight - container.clientHeight);
+    let place: Position = Position.none;
+    if (Position)
+    if (percent === 0) {
+      place = Position.reached_top;
+    } else if (percent === 1) {
+      place = Position.reached_bottom;
+    }
+    this.setState({
+      scroll_height: container.clientHeight * percent,
+      place: place
+    });
+  }
+  componentDidMount() {
+    window.addEventListener('scroll', () => this.UpdateScrollBar(), true);
+  }
+  componentWillUnmount() {
+    window.removeEventListener('scroll', () => this.UpdateScrollBar(), true);
+  }
+
+  render() {
+    return (
+      <div id='portfolio-container'>
+        <h1>page: {this.state.current_page}</h1>
+        <li>0</li>
+        <li>1</li>
+        <li>2</li>
+        <li>3</li>
+        <li>4</li>
+        <li>5</li>
+        <li>6</li>
+
+        <div style={{ height: this.state.scroll_height }} className='portfolio-scroll' />
+      </div>
+    );
+  }
 }
