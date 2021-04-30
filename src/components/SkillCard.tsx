003528242d12f7f93c91f7890b1463b1b3a06f3d
watasuke102 <watasuke102@gmail.com>
2021-04-30 19:17:15 +0900

    update: ポートフォリオのSkillページ

---
+++ src/components/SkillCard.tsx
@@ -0,0 +1,25 @@
+/*!
+ * SkillCard.tsx
+ *
+ * CopyRight (c) 2021 Watasuke
+ * Email  : <watasuke102@gmail.com>
+ * Twitter: @Watasuke102
+ * This software is released under the MIT SUSHI-WARE License.
+ */
+
+import React from 'react';
+import '../styles/SkillCard.scss';
+
+interface Props {
+  name: string,
+  body?: string,
+}
+
+export default (props: Props) => {
+  return (
+    <div className='SkillCard-container'>
+      <p className='name'>{props.name}</p>
+      {props.body && <p className='body'>{props.body}</p>}
+    </div>
+  );
+}
--- src/pages/portfolio.tsx
+++ src/pages/portfolio.tsx
@@ -7,8 +7,9 @@
  * This software is released under the MIT SUSHI-WARE License.
  */
 
-import { StaticImage } from 'gatsby-plugin-image';
 import React from 'react';
+import { StaticImage } from 'gatsby-plugin-image';
+import SkillCard from '../components/SkillCard';
 import Container from '../components/PortfolioContainer';
 import '../styles/portfolio.scss'
 
@@ -16,6 +17,7 @@ export default () => {
   return (
     <div id='portfolio-container'>
       <Container>
+        {/*
 
         <div id='portfolio-0'>
           <h2>Hi there👋</h2>
@@ -49,12 +51,46 @@ export default () => {
           <p>2004年生まれの高専生です</p>
           <p>プログラミングでツールを作ったり、フロントエンド開発したりしています</p>
           <p>AR/MRを始めとするxRに興味があります</p>
+          <div className='next-page' />
         </div>
 
+        */}
+
         <div id='portfolio-2'>
-          <h2>Works</h2>
-          <li>3</li><li>4</li><li>5</li>
-          <li>0</li><li>1</li><li>2</li>
+          <h2>Skills</h2>
+          <div id='skill-container'>
+            <div className='categoly'>
+              <p className='heading'>Languages</p>
+              <SkillCard name='C/C++' body='使用歴が最も長い言語です' />
+              <SkillCard name='TypeScript' body='Reactと一緒に使います' />
+              <SkillCard name='HTML' />
+            </div>
+            <div className='categoly'>
+              <p className='heading'>Frameworks</p>
+              <SkillCard name='React' body='Next.js, Gatsbyを使ったことがあります' />
+              <SkillCard name='Qt' />
+              <SkillCard name='OpenSiv3D' />
+              <SkillCard name='Flutter' body='使用回数は少ないです' />
+            </div>
+            <div className='categoly'>
+              <p className='heading'>Tools</p>
+              <SkillCard name='VSCode' body='愛用しているエディタ' />
+              <SkillCard name='Linux' body='Arch Linuxを主に使用しています' />
+              <SkillCard name='Git' body='コミットなど基本的な知識はあります' />
+              <SkillCard name='GitHub' />
+              <SkillCard name='Docker' />
+            </div>
+            <div className='categoly'>
+              <p className='heading'>Other</p>
+              <SkillCard name='MySQL' />
+              <SkillCard name='CSS/SCSS' />
+              <SkillCard name='AviUtl' />
+              <SkillCard name='Blender' body='高度なモデリングはできません'/>
+              <SkillCard name='Go' body='興味があります' />
+              <SkillCard name='Rust' body='興味があります' />
+            </div>
+          </div>
+          <div className='next-page' />
         </div>
 
         <div>
