a381c7a34816067e4172dcf02d650a346187f545
watasuke102 <watasuke102@gmail.com>
2023-02-23 16:15:19 +0900

    fix: Entrypoint>引数名変更とimportのcommit漏れ

--- src/components/feature/portfolio/Entrypoint/Entrypoint.tsx
+++ src/components/feature/portfolio/Entrypoint/Entrypoint.tsx
@@ -4,6 +4,7 @@
 // Email  : <watasuke102@gmail.com>
 // Twitter: @Watasuke102
 // This software is released under the MIT SUSHI-WARE License.
+import {motion} from 'framer-motion';
 import React from 'react';
 import './Entrypoint.scss';
 
