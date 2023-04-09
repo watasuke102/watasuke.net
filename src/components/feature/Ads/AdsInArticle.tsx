475b3d6932ae7f9fd39b829651a6d85f2837cd89
watasuke102 <watasuke102@gmail.com>
2023-04-09 10:50:09 +0900

    update: Ads>開発環境用のダミー表示を追加

--- src/components/feature/Ads/AdsInArticle.tsx
+++ src/components/feature/Ads/AdsInArticle.tsx
@@ -6,6 +6,7 @@
 // This software is released under the MIT SUSHI-WARE License.
 import config from '@config';
 import React from 'react';
+import './AdsInArticle.scss';
 
 export const AdsInArticle = (): React.ReactElement => {
   React.useEffect(() => {
@@ -17,7 +18,7 @@ export const AdsInArticle = (): React.ReactElement => {
   }, []);
 
   if (config.adsenseId.length <= 0 || config.inArticleSlot.length <= 0) {
-    return <></>;
+    return <div className='AdsInArticle-dummy'>Ads here</div>;
   }
 
   return (
