bec63ae1cbde2c554392f99e91053b6ef75956cd
watasuke102 <watasuke102@gmail.com>
2024-07-14 16:15:30 +0900

    update: ExtractHeading>use unified

--- main/src/utils/ExtractHeading.ts
+++ main/src/utils/ExtractHeading.ts
@@ -4,38 +4,25 @@
 // Email  : <watasuke102@gmail.com>
 // Twitter: @Watasuke102
 // This software is released under the MIT or MIT SUSHI-WARE License.
+import remarkParse from 'remark-parse';
+import {unified} from 'unified';
+import {visit} from 'unist-util-visit';
 import Heading from '@mytypes/Heading';
 
-/*
-マークダウンから見出しだけを抽出する
-見出しとその大きさ？を格納する
-例: 
-`
-# Title
-this is normal text.
-## Head 2
-### Head 3
-`
-=>[
-  {size: 1, body: "Title"},
-  {size: 2, body: "Head 2"},
-  {size: 3, body: "Head 3"},
-]
-*/
+export default function ExtractHeading(md: string): Heading[] {
+  const heads: Heading[] = [];
 
-export default function ExtractHeading(str: string): Heading[] {
-  // 見出し要素を見つけるための正規表現
-  // 文頭が1個以上の# + 空白 + 何らかの文字列
-  const heading_regexp = /^#+ (.*)/;
-  // 内容を改行でstrip（行ごとに検証）filterで見出しの行だけ取り出し、
-  // # (見出しを示す記号）の個数を計算して削除（見出しの文字列だけ抽出）
-  return str
-    .split('\n')
-    .filter(str => str.match(heading_regexp))
-    .map(str => {
-      return {
-        size: str.match(/#/g)?.length ?? -1,
-        body: str.replace(heading_regexp, '$1'),
+  unified()
+    .use(remarkParse)
+    .use(() => {
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/explicit-module-boundary-types
+      return (tree: any) => {
+        visit(tree, 'heading', node => {
+          heads.push({size: node.depth, body: node.children[0].value});
+        });
       };
-    });
+    })
+    .process(md);
+
+  return heads;
 }
