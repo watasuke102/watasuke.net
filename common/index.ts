3942d83999ab128cf55029094a46a458c8f83dc9
watasuke102 <watasuke102@gmail.com>
2024-07-16 14:19:27 +0900

    add: main>function that classifies URL embed type

--- common/index.ts
+++ common/index.ts
@@ -1,5 +1,6 @@
 import {AdsInArticle} from './src/Ads';
+import {classify_url_embed_type, UrlEmbedType} from './src/ClassifyUrlEmbedType';
 import {Body} from './src/Markdown/Body/Body';
 import {color} from './src/css/color';
 
-export {color, Body as Markdown, AdsInArticle};
+export {color, Body as Markdown, AdsInArticle, classify_url_embed_type, UrlEmbedType};
--- common/src/Markdown/Link/Link.tsx
+++ common/src/Markdown/Link/Link.tsx
@@ -6,6 +6,7 @@
 // This software is released under the MIT or MIT SUSHI-WARE License.
 import {Components} from 'hast-util-to-jsx-runtime';
 import React from 'react';
+import {classify_url_embed_type} from '@watasuke.net/common';
 import {EmbedCardType, InnerEmbedCardType} from '../PropsComponent';
 
 export function Link(EmbedCard: EmbedCardType, InnerEmbedCard: InnerEmbedCardType) {
@@ -17,36 +18,33 @@ export function Link(EmbedCard: EmbedCardType, InnerEmbedCard: InnerEmbedCardTyp
     if (link_text !== props.href || props.node.position?.start.column !== 1) {
       return <a {...props.node.properties}>{link_text}</a>;
     }
-    // Twitter
-    if ((props.href?.slice(0, 20) ?? '') === 'https://twitter.com/') {
-      return (
-        <blockquote className='twitter-tweet' data-theme='dark'>
-          <span>
-            <a href={props.href}>ツイート</a>を読み込み中...
-          </span>
-        </blockquote>
-      );
-    }
-    // YouTube
-    if ((props.href?.slice(0, 32) ?? '') === 'https://www.youtube.com/watch?v=') {
-      return (
-        <div className='youtube-wrapper'>
-          <iframe
-            src={`https://www.youtube.com/embed/${props.href?.slice(32)}`}
-            allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture'
-            title='YouTube video player'
-            allowFullScreen
-          ></iframe>
-        </div>
-      );
-    }
 
-    const inner_url = props.href?.match(/https:\/\/watasuke.net\/blog\/article\/(.+)/) ?? '';
-    if (inner_url) {
-      return <InnerEmbedCard slug={inner_url[1].replace('/', '')} />;
+    const embed_type = classify_url_embed_type(props.href ?? '');
+    switch (embed_type.embed_type) {
+      case 'internal':
+        return <InnerEmbedCard slug={embed_type.data} />;
+      case 'twitter':
+        return (
+          <blockquote className='twitter-tweet' data-theme='dark'>
+            <span>
+              <a href={props.href}>ツイート</a>を読み込み中...
+            </span>
+          </blockquote>
+        );
+      case 'youtube':
+        return (
+          <div className='youtube-wrapper'>
+            <iframe
+              src={`https://www.youtube.com/embed/${embed_type.data}`}
+              allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture'
+              title='YouTube video player'
+              allowFullScreen
+            ></iframe>
+          </div>
+        );
+      case 'none':
+        return <EmbedCard url={props.href ?? ''} />;
     }
-
-    return <EmbedCard url={props.href ?? ''} />;
   };
   return link;
 }
