ae2df8e8d353a17863bbbb0bd1b53477fb7e5ec8
watasuke102 <watasuke102@gmail.com>
2024-07-17 16:59:00 +0900

    fix: AddFootnoteLabel>title depends on the order

--- common/src/Markdown/plugins/AddFootnoteLabel.ts
+++ common/src/Markdown/plugins/AddFootnoteLabel.ts
@@ -6,17 +6,16 @@
 // This software is released under the MIT or MIT SUSHI-WARE License.
 import {visit} from 'unist-util-visit';
 
-const map = new Map<number, string>();
+const map = new Map<string, string>();
 export const remarkAddFootnoteLabel = () => {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/explicit-module-boundary-types
   return (tree: any) => {
     map.clear();
-    let counter = -1;
     visit(tree, 'footnoteDefinition', node => {
-      ++counter;
+      const id = `user-content-fnref-${node.identifier}`;
       visit(node, child => {
         if (child.value) {
-          map.set(counter, (map.get(counter) ?? '') + child.value);
+          map.set(id, (map.get(id) ?? '') + child.value);
         }
       });
     });
@@ -26,14 +25,12 @@ export const remarkAddFootnoteLabel = () => {
 export const rehypeAddFootnoteLabel = () => {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   return (tree: any) => {
-    let counter = 0;
     visit(tree, 'element', node => {
       const aria_described_by = node.properties.ariaDescribedBy ?? [''];
       if (aria_described_by[0] !== 'footnote-label') {
         return;
       }
-      node.properties['title'] = map.get(counter);
-      ++counter;
+      node.properties['title'] = map.get(node.properties.id);
     });
   };
 };
