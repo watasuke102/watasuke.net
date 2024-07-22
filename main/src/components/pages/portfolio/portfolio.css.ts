815c424e5cf0961ebd3e3bafc62e1ef57ea41d05
watasuke102 <watasuke102@gmail.com>
2024-07-22 14:14:04 +0900

    update: portfolio>add media query

--- main/src/components/pages/portfolio/portfolio.css.ts
+++ main/src/components/pages/portfolio/portfolio.css.ts
@@ -7,7 +7,7 @@
 import {style} from '@vanilla-extract/css';
 import {color} from '@watasuke.net/common/src/css/color';
 
-const header_height = 60;
+export const header_height = 72;
 export const main_container = style({
   paddingTop: header_height,
 });
@@ -21,12 +21,17 @@ export const header = style({
   display: 'grid',
   gridTemplateColumns: '1fr 1fr 1fr',
   alignItems: 'center',
-  paddingBlock: 8,
+  paddingBlock: 12,
   paddingRight: 8,
   fontSize: '1.2em',
   borderBottom: `dotted 1px ${color.fg}`,
   backgroundColor: `${color.bg}a0`,
   backdropFilter: 'blur(3px)',
+  '@media': {
+    'screen and (width < 950px)': {
+      gridTemplateColumns: 'auto 1fr auto',
+    },
+  },
 });
 
 export const animation_form = style({
@@ -45,4 +50,18 @@ export const translate_button = style({
   marginLeft: 'auto',
   border: `1px solid ${color.fg}`,
   boxShadow: '2px 2px 4px 2px #333',
+  '@media': {
+    'screen and (width < 600px)': {
+      gridTemplateColumns: '32px',
+      gap: 0,
+      width: 'auto',
+    },
+  },
+});
+export const button_text = style({
+  '@media': {
+    'screen and (width < 600px)': {
+      display: 'none',
+    },
+  },
 });
