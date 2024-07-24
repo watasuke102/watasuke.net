e7a83dbf84f9d95df06aa683944c888faa8b45cf
watasuke102 <watasuke102@gmail.com>
2024-07-25 08:03:34 +0900

    update: TocMapper>use window.scrollTo() instead of inner link
    prevent `Failed to execute 'removeChild' on 'Node'` error

--- main/src/components/feature/TableOfContents/TocMapper/TocMapper.css.ts
+++ main/src/components/feature/TableOfContents/TocMapper/TocMapper.css.ts
@@ -5,6 +5,7 @@
 // Twitter: @Watasuke102
 // This software is released under the MIT or MIT SUSHI-WARE License.
 import {style} from '@vanilla-extract/css';
+import {color} from '@watasuke.net/common/src/css/color';
 
 export const list_wrapper = style({
   margin: 0,
@@ -13,6 +14,14 @@ export const list_wrapper = style({
 export const item = style({
   listStyleType: 'decimal-leading-zero',
   marginBlock: 4,
+  textDecoration: 'underline',
+  color: color.p0,
+  '::marker': {
+    color: color.fg,
+  },
+  ':hover': {
+    cursor: 'pointer',
+  },
 });
 
 export const toc_list = [
--- main/src/components/feature/TableOfContents/TocMapper/TocMapper.tsx
+++ main/src/components/feature/TableOfContents/TocMapper/TocMapper.tsx
@@ -15,11 +15,21 @@ interface Props {
 export function TocMapper(props: Props): React.ReactElement {
   return (
     <ol className={css.list_wrapper}>
-      {props.headings.map(item => (
-        <li key={item.slug} className={`${css.item} ${css.toc_list[item.size]}`}>
-          <a href={`#${item.slug}`}>{item.body}</a>
-        </li>
-      ))}
+      {props.headings.map(item => {
+        const jump = () => {
+          const heading = document.getElementById(item.slug);
+          if (heading) {
+            window.scrollTo({top: heading.offsetTop, behavior: 'smooth'});
+          }
+        };
+        return (
+          <li key={item.slug} className={`${css.item} ${css.toc_list[item.size]}`}>
+            <span role='link' tabIndex={0} onClick={jump} onKeyDown={jump}>
+              {item.body}
+            </span>
+          </li>
+        );
+      })}
     </ol>
   );
 }
