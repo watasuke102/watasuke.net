48a74814aa17f3846188c72cfc2176226233fbe2
watasuke102 <watasuke102@gmail.com>
2025-01-13 10:12:54 +0900

    update: prettier>use mjs to use completion

---
+++ .prettierrc.mjs
@@ -0,0 +1,13 @@
+/**
+ * @type {import("prettier").Config}
+ */
+const config = {
+  tabWidth: 2,
+  printWidth: 80,
+  singleQuote: true,
+  arrowParens: "avoid",
+  trailingComma: "all",
+  jsxSingleQuote: true,
+  bracketSpacing: false,
+};
+export default config;
