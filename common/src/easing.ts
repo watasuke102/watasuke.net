98c30e834e96464433dfe351b6ffe5714342918b
watasuke102 <watasuke102@gmail.com>
2025-06-12 00:05:14 +0900

    update: common>easing is now properly typed

--- common/src/easing.ts
+++ common/src/easing.ts
@@ -5,15 +5,19 @@
 // Twitter: @watasuke1024
 // This software is released under the MIT or MIT SUSHI-WARE License.
 
-const easing_list: Record<string, number[]> = {
+const easing_list = {
   out_quint: [0.22, 1, 0.36, 1],
   out_expo: [0.16, 1, 0.3, 1],
   out_circ: [0, 0.55, 0.45, 1],
-};
+} satisfies Record<string, number[]>;
 
-type Easing = Record<string, {array: number[]; cubic_bezier: string}>;
+type Easing = Record<
+  keyof typeof easing_list,
+  {array: number[]; cubic_bezier: string}
+>;
 
-export const easing: Easing = Object.keys(easing_list).reduce((acc, key) => {
+export const easing: Easing = Object.keys(easing_list).reduce((acc, cur) => {
+  const key = cur as keyof typeof easing_list;
   const array = easing_list[key];
   acc[key] = {
     array,
