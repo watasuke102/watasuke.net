8b14e15f9abf17a77e8ecec444f83b8e7a2e80a3
watasuke102 <watasuke102@gmail.com>
2025-06-14 17:51:41 +0900

    update: portfolio>add End section

---
+++ main/src/components/pages/portfolio/sections/End/End.css.ts
@@ -0,0 +1,12 @@
+// watasuke.net
+// CopyRight (c) 2021-2025 watasuke
+//
+// Email  : <watasuke102@gmail.com>
+// Twitter: @watasuke1024
+// This software is released under the MIT or MIT SUSHI-WARE License.
+import {style} from '@vanilla-extract/css';
+
+export const container = style({
+  paddingBottom: 52, // for floating button
+  textAlign: 'center',
+});
---
+++ main/src/components/pages/portfolio/sections/End/End.tsx
@@ -0,0 +1,18 @@
+// watasuke.net
+// CopyRight (c) 2021-2025 watasuke
+//
+// Email  : <watasuke102@gmail.com>
+// Twitter: @watasuke1024
+// This software is released under the MIT or MIT SUSHI-WARE License.
+import * as css from './End.css';
+import {SimpleInnerLinks} from '@common';
+
+export function End() {
+  return (
+    <section className={css.container}>
+      <h2>Thank you for visiting my portfolio!</h2>
+      <p>How about another page?</p>
+      <SimpleInnerLinks />
+    </section>
+  );
+}
