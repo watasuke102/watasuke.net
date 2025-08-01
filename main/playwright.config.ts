4f270796f19b59ae6901137cfde2494669352873
watasuke102 <watasuke102@gmail.com>
2025-08-01 22:22:01 +0900

    update: add `open: 'never'` to playwright config

--- main/playwright.config.ts
+++ main/playwright.config.ts
@@ -4,14 +4,14 @@
 // Email  : <watasuke102@gmail.com>
 // Twitter: @watasuke1024
 // This software is released under the MIT or MIT SUSHI-WARE License.
-import {defineConfig, devices} from '@playwright/test';
+import { defineConfig, devices } from '@playwright/test';
 
 export default defineConfig({
   testDir: './tests',
   fullyParallel: true,
   forbidOnly: false,
   retries: 0,
-  reporter: 'html',
+  reporter: [['html', { open: 'never' }]],
   use: {
     trace: 'on-first-retry',
   },
@@ -19,24 +19,24 @@ export default defineConfig({
     // desktop
     {
       name: 'chromium',
-      use: {...devices['Desktop Chrome']},
+      use: { ...devices['Desktop Chrome'] },
     },
     {
       name: 'firefox',
-      use: {...devices['Desktop Firefox']},
+      use: { ...devices['Desktop Firefox'] },
     },
     {
       name: 'webkit',
-      use: {...devices['Desktop Safari']},
+      use: { ...devices['Desktop Safari'] },
     },
     // mobile
     {
       name: 'Mobile Chrome',
-      use: {...devices['Pixel 7']},
+      use: { ...devices['Pixel 7'] },
     },
     {
       name: 'Mobile Safari',
-      use: {...devices['iPhone 12']},
+      use: { ...devices['iPhone 12'] },
     },
   ],
 });
